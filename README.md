b174271e6a9009130dd551b5c9ae99a783483c0f
Zied <73042811+ZiedDev@users.noreply.github.com>
2023-07-29 23:59:31 +0300

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # june-ol-date
+
+This is a website I made Because I was bored and wanted to make something small but useful
+
+## Contact me
+The best way to reach out to me is via discord. 😉
+
+My discord: [ohzied](https://discordapp.com/users/484808856128585750)
---
+++ script.js
@@ -0,0 +1,30 @@
+const countDownTimer = document.getElementById("count-down-timer");
+
+// Set the date we're counting down to
+let countDownDate = new Date(1692162000000).getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function () {
+
+    // Get today's date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    countDownTimer.innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        countDownTimer.innerHTML = "Results are live";
+    }
+}, 1000);
