d01a4ecdc852f9881eddfa4cd35e882f0581e3e4
ZiedDev <73042811+ZiedDev@users.noreply.github.com>
2023-08-04 02:21:43 +0300

    v1.2 Added red color when it is last 24h Added a love this content icon (not functional atm)

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const countDownTimer = document.getElementById("count-down-timer");
+const resultsLink = '<a class="results-link" target="_blank" href="https://myresults.cie.org.uk/">this page</a>';
+
+const heartIcon = document.querySelector(".love-icon-internal");
+
+const showLoveConfirm = document.querySelector(".show-love-confirm");
 
 // Set the date we're counting down to
 let countDownDate = new Date(1692162000000).getTime();
 
 // Update the count down every 1 second
-let x = setInterval(function () {
+let updateCountDown = setInterval(function () {
 
     // Get today's date and time
     let now = new Date().getTime();
@@ -22,9 +27,50 @@ let x = setInterval(function () {
     countDownTimer.innerHTML = days + "d " + hours + "h "
         + minutes + "m " + seconds + "s ";
 
+    if (distance <= 86400000) {
+        countDownTimer.classList.add("box-red");
+    }
+
     // If the count down is finished, write some text
     if (distance < 0) {
-        clearInterval(x);
-        countDownTimer.innerHTML = "Results are live";
+        clearInterval(updateCountDown);
+        countDownTimer.classList.remove("box-red");
+        countDownTimer.innerHTML = `Results should be live. Visit ${resultsLink} to view them`;
     }
 }, 1000);
+
+let iLovedIt;
+
+if (document.cookie.includes("iLovedIt=true")) {
+    iLovedIt = true;
+    heartIcon.style.fill = '#ffffff';
+} else {
+    iLovedIt = false;
+    heartIcon.style.fill = '#none';
+}
+
+function sendHeart() {
+    if (!iLovedIt) {
+        heartIcon.style.fill = '#ffffff';
+        loveSentMessageIn();
+        setTimeout(loveSentMessageOut, 2000);
+        document.cookie = "iLovedIt=true";
+        iLovedIt = true;
+
+    } else {
+        heartIcon.style.fill = "none";
+        loveSentMessageOut();
+        document.cookie = "iLovedIt=false";
+        iLovedIt = false;
+    }
+}
+
+function loveSentMessageIn() {
+    showLoveConfirm.classList.add("confirm-like-slide-in-class");
+}
+function loveSentMessageOut() {
+    showLoveConfirm.classList.remove("confirm-like-slide-in-class");
+}
+
+// ----------------------------------
+
